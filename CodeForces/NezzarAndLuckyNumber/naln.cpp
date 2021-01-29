0ba49a759294aa51b68411b5fb5b9d9c5b341de3
Jad Isaac <67189765+jad-debugs@users.noreply.github.com>
2021-01-28 16:01:03 -0800

    update

---
+++ CodeForces/NezzarAndLuckyNumber/naln.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+ 
+int main()
+{
+    int t; cin >> t;
+ 
+    while(t--) {
+        int n, d; cin >> n >> d;
+ 
+        int arr[n];
+        int maxN = 0;
+ 
+        for(int i = 0; i < n; i++) {
+            cin >> arr[i];
+            string ans = "NO\n";
+            for(int j = 1; j <= 1000; j++) {
+                if((d*j <= arr[i]) && ((arr[i] - d*j)%10 == 0)) {
+                    ans = "YES\n";
+                    break;
+                }
+            }
+            cout << ans;
+        }
+ 
+ 
+    }
+ 
+    return 0;
+}
