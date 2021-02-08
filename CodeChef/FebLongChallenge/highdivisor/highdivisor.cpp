4b42ba5ea2c6ba6b52ce5c8d46f55b4c01c091ab
Jad Isaac <67189765+jad-debugs@users.noreply.github.com>
2021-02-08 10:18:40 -0800

    p1 of feb long

---
+++ CodeChef/FebLongChallenge/highdivisor/highdivisor.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n; cin >> n;
+
+    for(int i = 10; i >= 1; i--) {
+        if(n%i == 0) {
+            cout << i;
+            break;
+        }
+    }
+
+    return 0;
+}
