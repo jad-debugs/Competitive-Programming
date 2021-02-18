41befdee27c615c28c1c4acdbb0207dea6dec7de
Jad Isaac <67189765+jad-debugs@users.noreply.github.com>
2021-02-17 21:22:14 -0800

    CF Round #435 Div. 2 - B
    Bipartite tree is given. Goal is to add edges, while keeping this graph as a bipartite graph. Just  bicolor the graph into two groups, I will call them g1 and g2. The answer is g1*g2 - nodes -1. This graph was given as a tree, meaning we originally have nodes - 1 edges. The graph can have a max of g1*g2 edges by simple counting. New edges are just found by all edges - old edges.

---
+++ CodeForces/MahmoudandEhabandTheBipartieness/waytoolongname.cpp
@@ -0,0 +1,66 @@
+// Author : Jad Isaac
+// ID: jadDebugs
+// TASK: -----
+// LANG: C++                 
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define f first
+#define s second
+#define pii pair<int, int>
+
+void setIO(string name = "") {
+    ios_base::sync_with_stdio(0); cin.tie(0);
+    
+    /*
+    if ((int)(name).size()) {
+        freopen((name+".in").c_str(), "r", stdin);
+        freopen((name+".out").c_str(), "w", stdout);
+    }
+    */
+}
+
+const int hi = 2e5;
+vector<int> graph[hi];
+bool visited[hi];
+ll g1 = 0, g2 = 0;
+
+void dfs(int node, bool cur)
+{
+    if(visited[node]) return;
+
+    visited[node] = true;
+
+    if(!cur)
+        g1++;
+    else
+        g2++;
+
+    for(int u: graph[node]) {
+        if(cur)
+            dfs(u, 0);
+        else
+            dfs(u, 1);
+    }
+}
+
+int main()
+{
+    setIO();
+
+    int n; cin >> n;
+
+    for(int i = 0; i < n-1; i++) {
+        int a, b; cin >> a >> b;
+        graph[a].push_back(b);
+        graph[b].push_back(a);
+    }
+
+    dfs(1, 0);
+
+    cout << g1 * g2 - (n-1);
+
+    return 0;
+}
