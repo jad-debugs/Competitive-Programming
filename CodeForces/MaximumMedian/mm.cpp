73d6d75aca0b4e9949df5b481e006eb2c3a8a5f9
Jad Isaac <67189765+jad-debugs@users.noreply.github.com>
2021-02-22 18:20:14 -0800

    cf #577 div 2 C - bin search on ans
    First, realize that the number of moves needed to get to a certain median is (k = n/2) sigma (n) of max(0, x- arr[k]). After, bin search on ans by testing data sets between 1 and 2e9. I originally made a mistake by doing least + (greatest - least)/2, because if left == 0 and right == 1, this results in a infinite loop. The fix was to add one, to have the mid == 1, which would cause the code to terminate, giving us our answer.

--- CodeForces/MaximumMedian/mm.cpp
+++ CodeForces/MaximumMedian/mm.cpp
@@ -1,37 +1,62 @@
-#include <iostream>
-#include <algorithm>
+// Author : Jad Isaac
+// ID: jadDebugs
+// TASK: -----
+// LANG: C++                 
+ 
+#include <bits/stdc++.h>
 using namespace std;
-
+ 
 #define ll long long
-
-int n, k
-const int hi = 2e9;
+#define f first
+#define s second
+#define pii pair<int, int>
+ 
+ll n, k;
+const int hi = 2e5;
 int arr[hi];
 
-int works() 
+bool solve(int median)
 {
-     
-}
-
-bool findHalf() 
-{
-    for(int i = hi; i >= 1; i/=2) {
-        if(works())
+    ll moves = 0;
+    for(int i = n/2; i < n; i++) {
+        moves += max(0, median - arr[i]);
     }
-} 
-
+    if(moves <= k)
+        return true;
+    return false;
+}
+ 
+void setIO(string name = "")
+{
+    ios_base::sync_with_stdio(0); cin.tie(0);
+    
+    if ((int)(name).size()) {
+        freopen((name+".in").c_str(), "r", stdin);
+        freopen((name+".out").c_str(), "w", stdout);
+    } 
+}
+ 
+ 
 int main()
 {
+    setIO();
+ 
     cin >> n >> k;
 
     for(int i = 0; i < n; i++)
         cin >> arr[i];
+    sort(arr, arr+n);
     
-    sort(arr, arr+n); 
+    ll least = 1;
+    ll greatest = (int)2e9;
 
-    for(int i = 0; i < n; i++) {
-          
+    while(least < greatest) {
+        ll mid = least + (greatest - least + 1)/2;
+        if(solve(mid))
+            least = mid;
+        else
+            greatest = mid - 1;
     }
-
+    cout << least;
     return 0;
 }
