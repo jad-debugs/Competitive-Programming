759100574262b8e4a51799064c44f66047318872
Jad Isaac <67189765+jad-debugs@users.noreply.github.com>
2021-03-02 18:06:38 -0800

    fixed subarry divisibility - prefix sums
    I almost had the right solution a while ago. I didn't check to make sure the answer was positive and I didnt correctly take nC2 for all the values. Seems I was blind. Dont know why I took so long to redo the problem.

--- CSE150/SubarrayDivisibility/sd.cpp
+++ CSE150/SubarrayDivisibility/sd.cpp
@@ -2,12 +2,12 @@
 // ID: jadDebugs
 // TASK: -----
 // LANG: C++                 
-
+ 
 #include <bits/stdc++.h>
 using namespace std;
-
+ 
 #define ll long long
-
+ 
 void setIO(string name = "") {
     ios_base::sync_with_stdio(0); cin.tie(0);
     
@@ -18,32 +18,30 @@ void setIO(string name = "") {
     }
     */
 }
-
-
+ 
+ 
 int main()
 {
     setIO();
-
+ 
     int n; cin >> n;
-
+ 
     ll pf = 0;
-
-    map<ll, int> m;
-
-    m[0] = 1;
-
+ 
+    ll mods[n] = {0};
+ 
     for(int i = 0; i < n; i++) {
         int a; cin >> a;
-        pf = (pf + a)%n;
 
-        m[pf]++;
+        pf += (a%n + n)%n;
+        mods[pf%n]++;
     }
-
-    int ans = 0;
-    for(pair<ll, int> x : m)
-        ans += x.second-1; 
-
+ 
+    ll ans = mods[0]; // is element == 0 (mod n), then that is a sub array
+    for(ll x: mods)
+        ans += x*(x-1)/2;
+        // faster way for nC2;
     cout << ans;
-
+ 
     return 0;
 }
