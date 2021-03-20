95dbb42e5b43e253ee84e5418309a24255327e91
Jad Isaac <67189765+jad-debugs@users.noreply.github.com>
2021-03-19 18:25:25 -0700

    set up

---
+++ CodeForces/Exams/exams.cpp
@@ -0,0 +1,31 @@
+// Author : Jad Isaac
+// ID: jadDebugs
+// TASK: -----
+// LANG: C++                 
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define f first
+#define s second
+#define pii pair<int, int>
+
+void setIO(string name = "") {
+    ios_base::sync_with_stdio(0); cin.tie(0);
+    
+    if ((int)(name).size()) {
+        freopen((name+".in").c_str(), "r", stdin);
+        freopen((name+".out").c_str(), "w", stdout);
+    }   
+}
+
+
+int main()
+{
+    setIO("");
+
+    
+    
+    return 0;
+}
