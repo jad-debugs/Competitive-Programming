373d8e0fdabec66806fb17f45ef2c3edf09f32d0
Jad Isaac <67189765+jad-debugs@users.noreply.github.com>
2021-03-21 22:11:59 -0700

    Vacations - Greedy
    We keep track of what is behind us. If the number behind us is equal to us and isnt 3, or the current number is 0, we add one to our answer. If we got a 3, we just need to make it 2 if 1, and 1 if 2. Otherwise, it will eventually become a 0. Update behind other wise.

---
+++ CodeForces/Vacations/vacations.cpp
@@ -0,0 +1,52 @@
+// Author : Jad Isaac
+// ID: jadDebugs
+// TASK: -----
+// LANG: C++                 
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define f first
+#define s second
+#define pii pair<int, int>
+
+void setIO(string name = "") {
+    ios_base::sync_with_stdio(0); cin.tie(0);
+    
+    if ((int)(name).size()) {
+        freopen((name+".in").c_str(), "r", stdin);
+        freopen((name+".out").c_str(), "w", stdout);
+    }   
+}
+
+
+int main()
+{
+    setIO("");
+
+    int n; cin >> n;
+
+    int behind = 3, ans = 0;
+    for (int i = 0; i < n; i++) {
+
+        int x; cin >> x;
+        if (!x || (x == behind && x != 3)) {
+            ans++;
+            behind = 0;
+        }
+        else if (x == 3)
+            behind = 3 - behind;
+        else
+            behind = x;
+    }
+
+    cout << ans;
+
+    return 0;
+}
+
+/*
+100
+3 2 3 3 3 2 3 1 3 2 2 3 2 3 3 3 3 3 3 1 2 2 3 1 3 3 2 2 2 3 1 0 3 3 3 2 3 3 1 1 3 1 3 3 3 1 3 1 3 0 1 3 2 3 2 1 1 3 2 3 3 3 2 3 1 3 3 3 3 2 2 2 1 3 1 3 3 3 3 1 3 2 3 3 0 3 3 3 3 3 1 0 2 1 3 3 0 2 3 3
+*/
