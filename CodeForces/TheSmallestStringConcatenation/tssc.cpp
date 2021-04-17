47006a630c30dcfae1aa9419288a486284d287e7
Jad Isaac <67189765+jad-debugs@users.noreply.github.com>
2021-04-16 23:50:39 -0700

    observation and sorting
    honestly surprised it worked. My initial thoughts were to just append the latest character to make all the strings have the same size, but i couldnt find a way to do this in time (but it should work). Then, I realized I could basically brute force this. When choosing which strings to use, would I want to add string a first, or string b first. Well, just add them in that order and use the smaller (lexicographically) one.  This ended up working. The solution has an actual proof, but i didnt really understand it, but i also didnt thoroughly study it. This is the highest rated problem ive done so far (1700), but it def didnt feel harder 1500.

---
+++ CodeForces/TheSmallestStringConcatenation/tssc.cpp
@@ -0,0 +1,40 @@
+// Author : Jad Isaac
+// ID: jadDebugs
+// TASK: -----
+// LANG: C++                 
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define f first
+#define s second
+#define pii pair<int, int>
+
+int MX = 0;
+
+bool cmp(string a, string b)
+{
+    return a +b < b + a;
+}
+
+void setIO(string name = "") {
+    ios_base::sync_with_stdio(0); cin.tie(0);
+}
+
+
+int main()
+{
+    setIO("");
+
+    int n; cin >> n;
+    vector<string> arr(n);
+
+    for (int i = 0; i < n; i++)
+        cin >> arr[i];
+    sort(arr.begin(), arr.end(), cmp);
+
+    for (int i = 0; i < n; i++)
+        cout << arr[i];
+    return 0;
+}
