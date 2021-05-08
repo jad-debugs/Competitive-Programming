8624073d5c9e14174cc443ec4e4ea7915c372379
Jad Isaac <67189765+jad-debugs@users.noreply.github.com>
2021-05-07 22:57:01 -0700

    cf

---
+++ CodeForces/M-arrays/m.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t; cin >> t;
+    while (t--) {
+        int n, mod; cin >> n >> mod;
+        map<int, int> map;
+
+        for (int i = 0; i < n; i++) {
+            int a; cin >> a;
+            map[a%mod]++;
+        }
+        int ans = (map[0] > 0 ? 1: 0);
+        for (auto pii: map) {
+            if (pii.first == 0)
+                continue;
+            else if (abs(pii.second - map[mod - pii.second]) <= 1)
+                ans++;
+            else {
+                ans += abs(pii.second - map[m - pii.first]);
+            }
+            map[m-pii.first] = 0;
+        }
+        map.erase(map.find(m - pii.first));
+    }
+    return 0;
+}
