febd4aaebe62964f73e84d202f7c9d0b5a372ea6
Jad Isaac <67189765+jad-debugs@users.noreply.github.com>
2021-05-24 23:34:57 -0700

    finally got around to fixing this ff problem
    if you are wondering what the mistake was:
    the main mistake was on lines 30 and 31, we did not check for if we had visited this, as well as it being on, since it is possible we dont. so if the original check had included it prob would have worked, i cleaned it up a little, along with the rest of the code

--- USACO/SilverProblems/SwitchingOnTheLights/sotl.cpp
+++ USACO/SilverProblems/SwitchingOnTheLights/sotl.cpp
@@ -27,10 +27,8 @@ int diry[4] = {0, 0, -1, 1};
 bool pastVisited(pii node)
 {
     for (int i = 0; i < 4; i++) {
-        if (node.f + dirx[i] < 0 || node.f + dirx[i] >= n ||
-        node.s + diry[i] < 0 || node.s + diry[i] >= n)
-            continue;
-        if (matrix[node.f+dirx[i]][node.s+diry[i]])
+        int x = node.f+dirx[i], y = node.s+diry[i];
+        if (x >= 0 && x < n && y >= 0 && y < n && matrix[x][y] && visited[x][y])
             return true;
     }
     return false;
@@ -38,21 +36,21 @@ bool pastVisited(pii node)
 
 void dfs(pii node)
 {
-    if (visited[node.f][node.s] || node.f < 0 || node.f >= n || node.s < 0 || node.s >= n)
+    if (node.f < 0 || node.f >= n || node.s < 0 || node.s >= n || visited[node.f][node.s])
         return;
 
     visited[node.f][node.s] = 1;
     for (pii u: graph[node]) {
         if (!(matrix[u.f][u.s])) {
             matrix[u.f][u.s] = 1;
-            if (pastVisited(make_pair(u.f, u.s)))
-                dfs(make_pair(u.f, u.s));
+            if (pastVisited(u))
+                dfs(u);
         }
     }
     // ff
     for (int i = 0; i < 4; i++) {
         if (matrix[node.f+dirx[i]][node.s+diry[i]])
-            dfs(make_pair(node.f+dirx[i], node.s+diry[i]));
+            dfs({node.f+dirx[i], node.s+diry[i]});
     }
 }
 
@@ -66,17 +64,17 @@ int main()
     for (int i = 0; i < m; i++) {
         int x, y, a, b; cin >> x >> y >> a >> b;
         x--;y--;a--;b--;
-        graph[make_pair(x, y)].push_back(make_pair(a, b));
+        graph[{x, y}].push_back({a, b});
     }
 
     matrix[0][0] = 1;
 
-    dfs(make_pair(0, 0));
+    dfs({0, 0});
 
     int ans = 0;
 
-    for (int i = 0; i < hi; i++) {
-        for (int j = 0; j < hi; j++) {
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
             if (matrix[i][j]) {
                 ans++;
             }
@@ -86,3 +84,4 @@ int main()
     cout << ans << '\n';
     return 0;
 }
+
