ab88ac8248610f16a5a899a70b9311e316ff1bdc
Jad Isaac <67189765+jad-debugs@users.noreply.github.com>
2021-06-29 23:24:19 -0700

    not finished, 7/10

---
+++ USACO/SilverProblems/FairPhotography/fp.cpp
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define f first
+#define s second
+#define pii pair<int, int>
+
+int main()
+{
+    int n; cin >> n;
+
+    vector<pair<int, int>> cows(n+1, {0, 0});
+    vector<int> pf(n+1);
+
+    for (int i = 0; i < n; i++) {
+        int a; cin >> a;
+        char c; cin >> c;
+
+        if (c == 'G')
+            cows[i] = {a, 1};
+        else if (c == 'H')
+            cows[i] = {a, -1};
+    }
+
+    sort(cows.begin(), cows.end());
+
+    int ans = 0;
+
+    // accounting for continuous letter
+    for (int i = 1; i <= n; i++) {
+        int len = 1;
+        while (i+len <= n && cows[i+len].f == cows[i].f)
+            len++;
+        ans = max(ans, cows[len + i-1].f - cows[i].f);
+        i += len-1;
+    }
+
+    for (int i = 1; i <= n; i++) {
+        pf[i] = pf[i-1] + cows[i].s;
+    }
+
+    // fpos store cow after wher pf = pf
+    map<int, int> fPos;
+
+    // i corresponds to pf[i] in cow array.
+    for (int i = 1; i <= n; i++) {
+        if ((fPos.find(pf[i]) == fPos.end()) && i != n)
+            fPos[pf[i]] = cows[i+1].f;
+        else if (fPos.find(pf[i]) != fPos.end())
+            ans = max(ans, cows[i].f - fPos[pf[i]]);
+    }
+
+    cout << ans;
+
+    return 0;
+}
