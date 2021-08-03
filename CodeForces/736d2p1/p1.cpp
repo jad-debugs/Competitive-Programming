4b8bef0d3556378b4cdc4549ee27c9135d82623b
Jad Isaac <67189765+jad-debugs@users.noreply.github.com>
2021-08-02 19:14:33 -0700

    p1

---
+++ CodeForces/736d2p1/p1.cpp
@@ -0,0 +1,41 @@
+// Author : Jad Isaac
+// ID: jadDebugs
+// TASK: -----
+// LANG: C++                 
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define f first
+#define s second
+#define pii pair<int, int>
+
+void setIO(string name = "") {
+    ios_base::sync_with_stdio(0); cin.tie(0);
+    
+    if ((int)(name).size()) {
+        freopen((name+".in").c_str(), "r", stdin);
+        freopen((name+".out").c_str(), "w", stdout);
+    }   
+}
+
+
+int main()
+{
+    setIO("");
+
+    int t; cin >> t;
+
+    while (t--) {
+        int n; cin >> n;
+
+        n--;
+        if (n == 4)
+            cout << 2 << ' ' << 4 << '\n';
+        else
+            cout << 2 << ' ' << n/2 << '\n';
+    }
+
+    return 0;
+}
