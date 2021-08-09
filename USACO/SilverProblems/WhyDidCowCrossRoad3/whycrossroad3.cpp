e996ad8076132587b908d6e625895214448953cb
Jad Isaac <67189765+jad-debugs@users.noreply.github.com>
2021-08-08 18:23:59 -0700

    solved, simply if same connected component when not using roads, i overcomplicated

--- USACO/SilverProblems/WhyDidCowCrossRoad3/whycrossroad3.cpp
+++ USACO/SilverProblems/WhyDidCowCrossRoad3/whycrossroad3.cpp
@@ -11,40 +11,30 @@ using namespace std;
 #define s second
 #define pii pair<int, int>
 
-int n, k, z, color = 0, ans;
-// 1 -> k is color to check visited
-int visited[101][101];
-bool graph[101][101];
-set<pair<pii, pii>> distant, road;
+int n, k, z;
+bool visited[105][105];
+int graph[105][105];
 
-pii coor;
+set<pair<pii, pii>> road;
 
 int dx[4] = {1, -1, 0, 0};
 int dy[4] = {0, 0, 1, -1};
 
-void ff(int r, int c)
+void dfs(int r, int c, int color)
 {
     // not needed, but jic
-    if (r < 0 || r > n || c < 0 || c > n || visited[r][c] == color)
+    if (r <= 0 || r > n || c <= 0 || c > n || visited[r][c])
         return;
-    visited[r][c] = color;
+    
+    visited[r][c] = 1;
+    graph[r][c] = color;
 
     for (int i = 0; i < 4; i++) {
         int r2 = r + dx[i];
         int c2 = c + dy[i];
 
-        if (r2 < 0 || r2 > n || c2 < 0 || c2 > n || visited[r2][c2] == color)
-            continue;
-        if (road.find({{r2, c2}, {r, c}}) != road.end())
-            continue;
-
-        if (graph[r2][c2]) {
-            distant.insert({coor, {r2, c2}});
-            if (distant.find({{r2, c2}, coor}) != distant.end())
-                ans--;
-        }
-
-        ff(r2, c2);
+        if (road.find({{r, c}, {r2, c2}}) == road.end())
+            dfs(r2, c2, color);
     }
 }
 
@@ -60,35 +50,41 @@ void setIO(string name = "") {
 
 int main()
 {
-    // setIO("countcross");
+    setIO("countcross");
 
     cin >> n >> k >> z;
 
-    ans = (n*(n-1))/2;
+    int ans = 0;
 
-    vector<pii> cows;
+    vector<pii> cows(k);
 
     for (int i = 0; i < z; i++) {
         int r, c, r2, c2; cin >> r >> c >> r2 >> c2;
         road.insert({{r, c}, {r2, c2}});
+        road.insert({{r2, c2}, {r, c}});
     }
 
     for (int i = 0; i < k; i++) {
-        int r, c; cin >> r >> c;
-        graph[r][c] = 1;
-        cows.push_back({r, c});
+        cin >> cows[i].f >> cows[i].s;
     }
 
-    for (pii p: cows) {
-        color++;
-        coor = p;
-        ff(p.f, p.s);
+    int color = 0;
+    for (int i = 0; i < k; i++) {
+        if (!visited[cows[i].f][cows[i].s]) {
+            color++;
+            dfs(cows[i].f, cows[i].s, color);
+        }
     }
 
-    // for (auto x: distant)
-    //     cout << x.f.f << ' ' << x.f.s << " can reach " << x.s.f << ' ' << x.s.s << endl;
+    for (int i = 0; i < k; i++) {
+        for (int j = 0; j < i; j++) {
+            if (graph[cows[i].f][cows[i].s] != graph[cows[j].f][cows[j].s])
+                ans++;
+        }
+    }
 
     cout << ans;
 
     return 0;
 }
+
