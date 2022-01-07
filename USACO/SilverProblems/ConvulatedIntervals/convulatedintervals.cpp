85b8d41a1e62424fa2d7c4e4a683371469a3b176
Jad Isaac <67189765+jad-debugs@users.noreply.github.com>
2022-01-06 17:51:50 -0800

    im such a dumbass, coordinates start at 0 smh
    the diff between 2/20 and 20/20 im dead :(

--- USACO/SilverProblems/ConvulatedIntervals/convulatedintervals.cpp
+++ USACO/SilverProblems/ConvulatedIntervals/convulatedintervals.cpp
@@ -7,8 +7,8 @@ int main()
 {
     cin >> n >> m;
 
-    vector<long long> left(5001);
-    vector<long long> right(5001);
+    vector<long long> left(5005);
+    vector<long long> right(5005);
 
     for (int i = 0; i < n; i++) {
         int a, b; cin >> a >> b;
@@ -19,8 +19,8 @@ int main()
     vector<long long> pfl(1e6);
     vector<long long> pfr(1e6);
 
-    for (int i = 1; i < 5005; i++) {
-        for (int j = 1; j <= 5005; j++) {
+    for (int i = 0; i < 5004; i++) {
+        for (int j = 0; j <= 5004; j++) {
             pfl[i+j] += left[i]*left[j];
             pfr[i+j] += right[i]*right[j];
         }
